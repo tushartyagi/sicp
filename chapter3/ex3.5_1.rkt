560dcabf550223b4277932bf9a103a5d3ec797ab
Tushar Tyagi <tushar4r@gmail.com>
2016-01-15 11:24:53 +0530

    Fri, Jan 15, 2016 11:24:53 AM: Estimate Integral using unit circle/rect

--- chapter3/ex3.5_1.rkt
+++ chapter3/ex3.5_1.rkt
@@ -20,13 +20,21 @@
         (sqr r))))
 
 (define (random-in-range low high)
-  (let ((range (- high low)))
-    (+ low (random range))))
+  (+ low
+     (* (random) high)))
 
 (define P (create-circle-predicate 5 7 3))
+(define unit-pred (create-circle-predicate 0 0 1))
 
 ;; Taking the values as given in the book
 ;; Since estimate-integral gives area of a circle, need to divide this by
 ;; (sqr r) to get pi
-(/ (estimate-integral P 2 8 4 10 10000)
-   (* 1.0 (sqr 3)))
+(estimate-integral unit-pred 1.0 -1.0 1.0 -1.0 1000000)
+(/ (estimate-integral P 2 8 4 10 1000000)
+   (sqr 3.0))
+
+;; The biggest problem I face here is that the estimate-integral just doesn't
+;; work with anything other than unit circle and unit square. I calculate the
+;; area of the circle and divide it by (sqr r) but this gives some value which
+;; isn't pie. 
+   
