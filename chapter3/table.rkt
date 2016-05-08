95ea689a44fadf1cb27251820077e1029503ed94
Tushar Tyagi <tushar4r@gmail.com>
2016-05-08 17:55:40 +0530

    Reformatting

--- chapter3/table.rkt
+++ chapter3/table.rkt
@@ -42,23 +42,7 @@
 (define get (lambda (table k) ((table 'get) k)))
 (define show  (lambda (table) (table 'show)))
 
-(define polar (create-table))
-;; In polar representation: (2,4) = 2 + 4j
-(insert polar 'real-part (lambda (z) (car z)))
-(insert polar 'imag-part (lambda (z) (cdr z)))
 
-(define rectangular (create-table))
-;; In rectangular representation: (2,4) = (r, theta)
-(insert rectangular 'magnitude (lambda (z) (car z)))
-(insert rectangular 'angle (lambda (z) (cdr z)))
-
-
-;; interface for the function table
-;; this is a table working with 2 indices. The values of the first table are
-;; tables 
-(define function-table (create-table))
-(insert function-table 'polar polar)
-(insert function-table 'rectangular rectangular)
 
 ;; The way to access the functions stored in function table: 2 way redirection
 
@@ -81,16 +65,32 @@
 
 
 (define real-part (create-table))
-;; In real-part representation: (2,4) = 2 + 4j
+;; In polar, representation: (2,4) = 2 + 4j
+;; In rectangular, representation: (2,4) => 2 is the radius, 4 is the angle
 (insert real-part 'polar (lambda (z) (car z)))
 (insert real-part 'rectangular  (lambda (z) (* (car z) (cos (cdr z)))))
 
 (define imag-part (create-table))
-;; In imag-part representation: (2,4) = 2 + 4j
 (insert imag-part 'polar (lambda (z) (cdr z)))
 (insert imag-part 'rectangular  (lambda (z) (* (car z) (sin (cdr z)))))
+
+(define polar (create-table))
+;; In polar representation: (2,4) = 2 + 4j
+(insert polar 'real-part (lambda (z) (car z)))
+(insert polar 'imag-part (lambda (z) (cdr z)))
+
+(define rectangular (create-table))
+;; In rectangular representation: (2,4) = (r, theta)
+(insert rectangular 'magnitude (lambda (z) (car z)))
+(insert rectangular 'angle (lambda (z) (cdr z)))
+
+;; interface for the function table
+;; this is a table working with 2 indices. The values of the first table are
+;; tables 
+(define function-table (create-table))
+(insert function-table 'polar polar)
+(insert function-table 'rectangular rectangular)
 
 (define op-table (create-table))
 (insert op-table 'real-part real-part)
 (insert op-table 'imag-part imag-part)
-
